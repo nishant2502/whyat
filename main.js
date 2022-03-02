e9a46b62b03e00b1b0b10004bf5f4301ec4c7255
nishant2502 <80115029+nishant2502@users.noreply.github.com>
2022-03-02 16:37:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+img = "";
+
+function preload(){
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420)
+        canvas.center();
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#9400D3");
+    text("Dog", 45, 75);
+    noFill();
+    stroke("#9400D3");
+   rect(30, 60, 450, 350 );
+}
+
